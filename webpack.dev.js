d9906f8b188bee73216d25db3792420a49636bbe
Hidekazu Kobayashi <kobahide789@gmail.com>
2020-09-21 04:47:00 +0900

    Init project

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+});
